2dbe7ef539f783d5e633772f182aa077f7f21314
Luiz Filipe <luizamorim.dev@gmail.com>
2026-06-10 23:40:36 -0300

    adicionando configurações de entrada

--- bingo.js
+++ bingo.js
@@ -1,11 +1,33 @@
-// Variáveis do bingo
+// 1. configurações iniciais do jogo
 const TOTAL_NUMERO_SORTEADO = 75;
-let totalNumeroSorteado = 7;
 let nomeDoEvento = "Bingo da Alegria";
- 
+
+// 2. variáveis para o estado do jogo
+let numerosDisponiveis = [];
+let totalNumeroSorteado = [];
+
+// 3. cálculos do estado atual do jogo
 let restante = TOTAL_NUMERO_SORTEADO - totalNumeroSorteado;
 let resultadoJogo = (totalNumeroSorteado === TOTAL_NUMERO_SORTEADO);
+
+// 4. consigurações de entrada
+const readline = require("readline") 
+
+rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("Escolha uma opção: ", function (opcao){
+
+    console.log("Nome do Evento: ", nomeDoEvento);
+    console.log("Opção escolhida: ", opcao);
+    rl.close();
+
+
+})
+
+
  
-console.log("Nome do Evento: ", nomeDoEvento);
-console.log("Faltam: ", restante);
-console.log("Jogo Terminou: ", resultadoJogo);
+//console.log("Faltam: ", restante);
+//console.log("Jogo Terminou: ", resultadoJogo);
