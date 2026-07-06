9b4f6e0f027272e5075c1057e87d6dcc8dfdceef
Luiz Filipe <luizamorim.dev@gmail.com>
2026-07-06 20:09:44 -0300

    adicionando a função de reiniciar o jogo

--- bingo.js
+++ bingo.js
@@ -3,38 +3,41 @@ const TOTAL_NUMEROS_BINGO = 75;
 let nomeDoEvento = "Bingo da Alegria";
 
 //  variáveis para o estado do jogo
-let numerosDisponiveis = [];
-let jaSorteados = [];
+let numerosDisponiveis = []; 
+let jaSorteados = []; 
 
 // funções 
-
 // criar função de mensagem de abertura única para o jogo 
-
-
 
 function exibirMenu(){
     //console.log("\nBem-vindo ao Bingo da Alegria!");
-
-    console.log('Escolha uma opção do menu:');
-    console.log("\n1. Sortear um número");
+    console.log("1. Sortear um número");
     console.log("2. Verificar números sorteados");
     console.log("3. Verificar números restantes");
-    console.log("0. Sair do jogo");
+    console.log("4. Reiniciar jogo")
+    console.log("0. Sair do jogo\n");
 }
+
 
 function inicializarNumerosDisponiveis(){
     let numeros = [];
-
+                
     for (let numero = 1; numero <=TOTAL_NUMEROS_BINGO; numero++){
-        numeros.push(numero);
-    } // 1 - 75 
+        numeros.push(numero);     
+    } // 1, 2, 3 .... - 75 
 
     return numeros;
+}   
+
+function reiniciarJogo(){
+    numerosDisponiveis = inicializarNumerosDisponiveis();
+    jaSorteados = [];
+    console.log("\nO jogo foi reiniciado com sucesso!");
 }
 
 
 function calcularRestante(){
-    return TOTAL_NUMEROS_BINGO - jaSorteados.length;
+    return TOTAL_NUMEROS_BINGO - jaSorteados.length; 
 } // Essa função calcula quantos números ainda faltam ser sorteados.
 
 //function verificarResultadoJogo(){
@@ -48,14 +51,19 @@ function sortearNumero(){
     if(numerosDisponiveis.length === 0 ){
         return null;
     }
-   
+    // list = [2, 1, , 4, 5]
+    //         0  1  2  3  4
     let indiceAleatorio = Math.floor(Math.random() * numerosDisponiveis.length); // Gera um índice aleatório dentro do intervalo do array numerosDisponiveis 1
-
+                        // list[indiceAleatorio] = 3
     let numeroSorteado = numerosDisponiveis[indiceAleatorio]; // Obtém o número correspondente ao índice aleatório
 
     numerosDisponiveis.splice(indiceAleatorio, 1);
     
     jaSorteados.push(numeroSorteado);
+
+    jaSorteados.sort(function(a,b){
+        return a - b;
+    });
 
     return numeroSorteado;
 
@@ -73,7 +81,6 @@ const rl = readline.createInterface({
 });
 
 
-// perguntamos dentro da função - função para manter o menu repetindo 
 function iniciarMenu(){
 
     exibirMenu();
@@ -101,7 +108,9 @@ function iniciarMenu(){
             if(jaSorteados.length === 0){
                 console.log("\nNenhum número foi sorteado ainda.");
             } else {
-                console.log("\nNúmeros já sorteados: ", jaSorteados)
+                console.log("\nNúmeros já sorteados: ", jaSorteados);
+                console.log("Já sorteados: ", jaSorteados.length,"\n");
+
             }
 
             iniciarMenu();
@@ -109,6 +118,12 @@ function iniciarMenu(){
         } else if(opcao === "3"){
 
             console.log("\nNúmeros restantes: ", calcularRestante());
+
+            iniciarMenu();
+
+        } else if(opcao === "4"){
+
+            reiniciarJogo();
 
             iniciarMenu();
 
@@ -127,9 +142,6 @@ function iniciarMenu(){
     });
 }
 
-// chamando a função
-// iniciarMenu();
-
 function mensagemAbertura(){
     rl.question("Olá! Qual é o seu nome? ", function(nome) {
         console.log("\nBem-vindo(a), " + nome + ", ao " + nomeDoEvento + "!");
@@ -138,4 +150,3 @@ function mensagemAbertura(){
 }
 
 mensagemAbertura();
-       
